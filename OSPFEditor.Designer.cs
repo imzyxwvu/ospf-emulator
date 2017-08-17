85a98360b97851cadb64826581bd5d554e1f7502
zyxwvu Shi <syc@0xspot.com>
2017-08-17 15:48:13 +0800

    Initial commit.

---
+++ OSPFEditor.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace OSPF
+{
+    partial class OSPFEditor
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OSPFEditor));
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tscbSpeed = new System.Windows.Forms.ToolStripComboBox();
+            this.tsbDeployLine = new System.Windows.Forms.ToolStripButton();
+            this.mainToolStrip = new System.Windows.Forms.ToolStrip();
+            this.tsbDeployRouter = new System.Windows.Forms.ToolStripButton();
+            this.tsbDoSearch = new System.Windows.Forms.ToolStripButton();
+            this.tsbDeleteRouter = new System.Windows.Forms.ToolStripButton();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.mainToolStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(692, 495);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(692, 520);
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.toolStrip1);
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.mainToolStrip);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tscbSpeed,
+            this.tsbDeployLine});
+            this.toolStrip1.Location = new System.Drawing.Point(236, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(225, 25);
+            this.toolStrip1.TabIndex = 1;
+            // 
+            // tscbSpeed
+            // 
+            this.tscbSpeed.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.tscbSpeed.Items.AddRange(new object[] {
+            "Ethernet",
+            "FastEthernet",
+            "GigabitEthernet",
+            "TenGigabitEthernet"});
+            this.tscbSpeed.Name = "tscbSpeed";
+            this.tscbSpeed.Size = new System.Drawing.Size(121, 25);
+            // 
+            // tsbDeployLine
+            // 
+            this.tsbDeployLine.Image = ((System.Drawing.Image)(resources.GetObject("tsbDeployLine.Image")));
+            this.tsbDeployLine.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbDeployLine.Name = "tsbDeployLine";
+            this.tsbDeployLine.Size = new System.Drawing.Size(61, 22);
+            this.tsbDeployLine.Text = "拉网线";
+            // 
+            // mainToolStrip
+            // 
+            this.mainToolStrip.Dock = System.Windows.Forms.DockStyle.None;
+            this.mainToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbDeployRouter,
+            this.tsbDoSearch,
+            this.tsbDeleteRouter});
+            this.mainToolStrip.Location = new System.Drawing.Point(3, 0);
+            this.mainToolStrip.Name = "mainToolStrip";
+            this.mainToolStrip.Size = new System.Drawing.Size(217, 25);
+            this.mainToolStrip.TabIndex = 0;
+            // 
+            // tsbDeployRouter
+            // 
+            this.tsbDeployRouter.Image = ((System.Drawing.Image)(resources.GetObject("tsbDeployRouter.Image")));
+            this.tsbDeployRouter.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbDeployRouter.Name = "tsbDeployRouter";
+            this.tsbDeployRouter.Size = new System.Drawing.Size(85, 22);
+            this.tsbDeployRouter.Text = "放置路由器";
+            // 
+            // tsbDoSearch
+            // 
+            this.tsbDoSearch.Image = ((System.Drawing.Image)(resources.GetObject("tsbDoSearch.Image")));
+            this.tsbDoSearch.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbDoSearch.Name = "tsbDoSearch";
+            this.tsbDoSearch.Size = new System.Drawing.Size(73, 22);
+            this.tsbDoSearch.Text = "搜索路径";
+            // 
+            // tsbDeleteRouter
+            // 
+            this.tsbDeleteRouter.Enabled = false;
+            this.tsbDeleteRouter.Image = ((System.Drawing.Image)(resources.GetObject("tsbDeleteRouter.Image")));
+            this.tsbDeleteRouter.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbDeleteRouter.Name = "tsbDeleteRouter";
+            this.tsbDeleteRouter.Size = new System.Drawing.Size(49, 22);
+            this.tsbDeleteRouter.Text = "删除";
+            // 
+            // OSPFEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 520);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Name = "OSPFEditor";
+            this.Text = "OSPF Emulator";
+            this.Load += new System.EventHandler(this.OSPFEditor_Load);
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.mainToolStrip.ResumeLayout(false);
+            this.mainToolStrip.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.ToolStrip mainToolStrip;
+        private System.Windows.Forms.ToolStripButton tsbDeployRouter;
+        private System.Windows.Forms.ToolStripButton tsbDeleteRouter;
+        private System.Windows.Forms.ToolStripButton tsbDoSearch;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox tscbSpeed;
+        private System.Windows.Forms.ToolStripButton tsbDeployLine;
+
+    }
+}
+
---
+++ OSPFEditor.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OSPF
+{
+    public partial class OSPFEditor : Form
+    {
+        List<Router> routers = new List<Router>();
+        List<Dictionary<Router, int>> adjMat = new List<Dictionary<Router, int>>();
+        Router routerToDeploy;
+        Router routerHover;
+        Router routerSelected;
+        Router routerFrom;
+        Router routerSearchFrom;
+        Point mousePos = new Point();
+        Bitmap buffer;
+        Canvas canvas = new Canvas();
+
+        public OSPFEditor()
+        {
+            InitializeComponent();
+            this.Resize += new EventHandler(OSPFEditor_Resize);
+            canvas.Paint += new PaintEventHandler(canvas_Paint);
+            canvas.MouseMove += new MouseEventHandler(canvas_MouseMove);
+            canvas.MouseDown += new MouseEventHandler(canvas_MouseDown);
+            canvas.MouseUp += new MouseEventHandler(canvas_MouseUp);
+            tsbDeployRouter.Click += new EventHandler(tsbDeployRouter_Click);
+            tsbDeployLine.Click += new EventHandler(tsbDeployLine_Click);
+            tsbDeleteRouter.Click += new EventHandler(tsbDeleteRouter_Click);
+            tsbDoSearch.Click += new EventHandler(tsbDoSearch_Click);
+            toolStripContainer1.ContentPanel.Controls.Add(canvas);
+        }
+
+        void OSPFEditor_Resize(object sender, EventArgs e)
+        {
+            buffer.Dispose();
+            buffer = new Bitmap(canvas.Width, canvas.Height);
+        }
+
+        void tsbDoSearch_Click(object sender, EventArgs e)
+        {
+            if (tsbDoSearch.Checked)
+            {
+                routerSearchFrom = null;
+                tsbDoSearch.Checked = false;
+            }
+            else
+            {
+                tsbDeployLine.Checked = false;
+                tsbDeployRouter.Checked = false;
+                routerToDeploy = null;
+                if (routerFrom != null)
+                {
+                    routerSearchFrom = routerFrom;
+                    routerFrom = null;
+                    UpdateRouterCosts(routers.IndexOf(routerSearchFrom));
+                    tsbDoSearch.Checked = true;
+                }
+            }
+            canvas.Refresh();
+        }
+
+        void tsbDeleteRouter_Click(object sender, EventArgs e)
+        {
+            if (routerFrom != null)
+            {
+                for (int i = 0; i < routers.Count; i++)
+                {
+                    if (adjMat[i].ContainsKey(routerFrom))
+                        adjMat[i].Remove(routerFrom);
+                }
+                adjMat.RemoveAt(routers.IndexOf(routerFrom));
+                routers.Remove(routerFrom);
+                canvas.Refresh();
+                routerFrom = null;
+            }
+        }
+
+        void EnlargeAdjMat()
+        {
+            adjMat.Add(new Dictionary<Router, int>());
+        }
+
+        int CostOfLinkType()
+        {
+            switch (tscbSpeed.Text)
+            {
+                case "Ethernet":
+                    return 100;
+                case "FastEthernet":
+                    return 50;
+                case "GigabitEthernet":
+                    return 10;
+                case "TenGigabitEthernet":
+                    return 1;
+                default:
+                    return 1;
+            }
+        }
+
+        void EstablishLine(int from, Router to)
+        {
+            if (from != routers.IndexOf(to))
+            {
+                int cost = CostOfLinkType();
+                adjMat[from][to] = cost;
+                adjMat[routers.IndexOf(to)][routers[from]] = cost;
+            }
+        }
+
+        void EstablishLine(Router from, Router to)
+        {
+            EstablishLine(routers.IndexOf(from), to);
+        }
+
+        void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (!tsbDeployRouter.Checked)
+            {
+                routerSelected = routerHover;
+                if (tsbDeployLine.Checked && routerSelected != null)
+                {
+                    if (routerFrom != null)
+                    {
+                        EstablishLine(routerFrom, routerSelected);
+                    }
+                }
+                routerFrom = routerSelected;
+                tsbDeleteRouter.Enabled = routerFrom != null;
+            }
+            canvas.Refresh();
+        }
+
+        void canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (tsbDeployRouter.Checked && routerToDeploy != null)
+            {
+                routerToDeploy.Path = new List<int>();
+                routers.Add(routerToDeploy);
+                EnlargeAdjMat();
+                routerToDeploy = new Router();
+            }
+            routerSelected = null;
+            canvas.Refresh();
+        }
+
+        void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            routerHover = null;
+            for (int i = 0; i < routers.Count; i++)
+                if (routers[i].Hover(e.X, e.Y))
+                    routerHover = routers[i];
+            if (tsbDeployRouter.Checked)
+            {
+                if (routerToDeploy == null)
+                    routerToDeploy = new Router();
+                routerToDeploy.x = e.X;
+                routerToDeploy.y = e.Y;
+                canvas.Cursor = Cursors.Hand;
+            }
+            else
+            {
+                if (routerSelected == null)
+                {
+                    canvas.Cursor = routerHover == null ? Cursors.Arrow : Cursors.Hand;
+                }
+                else
+                {
+                    routerSelected.x = e.X;
+                    routerSelected.y = e.Y;
+                    canvas.Cursor = Cursors.Hand;
+                }
+            }
+            mousePos.X = e.X;
+            mousePos.Y = e.Y;
+            if(routerSelected != null || routerToDeploy != null || tsbDeployLine.Checked)
+                canvas.Refresh();
+        }
+
+        void tsbDeployLine_Click(object sender, EventArgs e)
+        {
+            if (tsbDeployLine.Checked = !tsbDeployLine.Checked)
+            {
+                tsbDeployRouter.Checked = false;
+                routerSearchFrom = null;
+                tsbDoSearch.Checked = false;
+            }
+            routerToDeploy = null;
+            canvas.Refresh();
+        }
+
+        void tsbDeployRouter_Click(object sender, EventArgs e)
+        {
+            if (tsbDeployRouter.Checked = !tsbDeployRouter.Checked)
+            {
+                tsbDeployLine.Checked = false;
+                routerSearchFrom = null;
+                tsbDoSearch.Checked = false;
+            }
+            else
+                routerToDeploy = null;
+            routerSelected = null;
+            canvas.Refresh();
+        }
+
+        public bool HasLinkBetween(int a, Router b)
+        {
+            if (!adjMat[a].ContainsKey(b))
+                return false;
+            return adjMat[a][b] < 10000;
+        }
+
+        public int CostBetween(int a, Router b)
+        {
+            if (!adjMat[a].ContainsKey(b))
+                return 10000;
+            return adjMat[a][b];
+        }
+
+        public int CostBetween(int a, int b)
+        {
+            return CostBetween(a, routers[b]);
+        }
+
+        public void UpdateRouterCosts(int src)
+        {
+            bool[] visited = new bool[routers.Count];
+            int visitedCount = 1;
+            int a = src;
+            for (int i = 0; i < routers.Count; i++)
+            {
+                routers[i].Path.Clear();
+                routers[i].Cost = 10000;
+                routers[i].Path.Add(a);
+                visited[i] = false;
+            }
+            routers[a].Cost = 0;
+            while (visitedCount < routers.Count)
+            {
+                visited[a] = true;
+                for (int i = 0; i < routers.Count; i++)
+                {
+                    if (!visited[i])
+                    {
+                        int cost = CostBetween(a, i);
+                        if (routers[a].Cost + cost < routers[i].Cost)
+                        {
+                            routers[i].Path.Clear();
+                            routers[i].Path.AddRange(routers[a].Path);
+                            routers[i].Path.Add(i);
+                            routers[i].Cost = routers[a].Cost + cost;
+                        }
+                    }
+                }
+                visitedCount++;
+                int lowestCost = int.MaxValue;
+                for (int i = 0; i < routers.Count; i++)
+                {
+                    if (routers[i].Cost < lowestCost && !visited[i])
+                    {
+                        lowestCost = routers[i].Cost;
+                        a = i;
+                    }
+                }
+            }
+        }
+
+        void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = Graphics.FromImage(buffer);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g.Clear(Color.White);
+            for (int i = 0; i < routers.Count; i++)
+            {
+                for (int j = i + 1; j < routers.Count; j++)
+                {
+                    int cost = CostBetween(i, j);
+                    if (cost < 10000)
+                    {
+                        if (tsbDoSearch.Checked && routerFrom != null && routerFrom.HasPathBetween(i, j))
+                            g.DrawLine(new Pen(Brushes.Red, 2), routers[i].ToPoint(), routers[j].ToPoint());
+                        else if (cost <= 10)
+                            g.DrawLine(new Pen(Brushes.Black, 2), routers[i].ToPoint(), routers[j].ToPoint());
+                        else
+                            g.DrawLine(Pens.Black, routers[i].ToPoint(), routers[j].ToPoint());
+                    }
+                }
+            }
+            if (tsbDeployLine.Checked && routerFrom != null)
+                g.DrawLine(Pens.DarkGray, routerFrom.ToPoint(), mousePos);
+            for (int i = 0; i < routers.Count; i++)
+            {
+                g.DrawImage(OSPF.Properties.Resources.Router, routers[i].x - 25, routers[i].y - 25, 50, 50);
+                if (routerFrom == routers[i])
+                    g.DrawEllipse(new Pen(Color.Navy, 2.0f), routers[i].x - 28, routers[i].y - 28, 56, 56);
+                else if(routerSearchFrom == routers[i])
+                    g.DrawEllipse(new Pen(Color.Magenta, 2.0f), routers[i].x - 28, routers[i].y - 28, 56, 56);
+                g.DrawString(i.ToString(), new Font("simsun", 12), Brushes.Gray, routers[i].x + 20, routers[i].y + 15);
+            }
+            if(routerToDeploy != null)
+                g.DrawImage(OSPF.Properties.Resources.Router, routerToDeploy.x - 25, routerToDeploy.y - 25, 50, 50);
+            e.Graphics.DrawImageUnscaled(buffer, 0, 0);
+            GC.Collect();
+        }
+
+        private void OSPFEditor_Load(object sender, EventArgs e)
+        {
+            buffer = new Bitmap(canvas.Width, canvas.Height);
+        }
+    }
+
+    public class Canvas : Control
+    {
+        public Canvas()
+        {
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            this.Dock = DockStyle.Fill;
+        }
+    }
+
+    public class Router
+    {
+        public int x;
+        public int y;
+        public List<int> Path;
+        public int Cost;
+
+        public bool Hover(int mx, int my)
+        {
+            return Math.Pow(mx - x, 2) + Math.Pow(my - y, 2) < 625;
+        }
+
+        public bool HasPathBetween(int i, int j)
+        {
+            if (!Path.Contains(i) || !Path.Contains(j))
+                return false;
+            return Math.Abs(Path.IndexOf(i) - Path.IndexOf(j)) == 1;
+        }
+
+        public Point ToPoint()
+        {
+            return new Point(x, y);
+        }
+    }
+}
